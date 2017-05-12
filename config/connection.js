7d94f816684719cf666eaeee3d2cf5e4b5abd70e
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-12 09:29:26 -0500

    preparing to deploy to Heroku, JAWS_DB added

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
 
-connection.connect(function(err){
-	if (err) throw err;
-	console.log('Connected to mySQL on thread', connection.threadId);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to mySQL on thread', connection.threadId);
 });
 
 module.exports = connection;
